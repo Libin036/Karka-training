827a3e6d6c009a5b65e5fde1f76fa89cb5161921
Libin036 <libin3636@gmail.com>
2023-08-23 12:17:16 +0530

    Adding new files

---
+++ javascript/introduction/script.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+
+
+let score = 0   //initialization
+//assignment
+score = 10
+
+console.log(score);
+
+let amount = 1000
+ 
+console.log("Your bill amount is "+amount);
+
+
+
+
+
+
